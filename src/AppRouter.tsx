9db5eaf771a680afd67ffb453a0e4f783d7334a6
MartinPPY <martinsantiago.se@gmail.com>
2025-10-15 18:34:14 -0300

    feat:quitando componentes que no se utilizaran

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Route } from "react-router-dom"
 import { RoutesWithNotFound } from "./components"
-import { ForgotPassword, Login, Register } from "./public"
+import { Login } from "./public"
 import type { ReactNode } from "react"
 
 interface Props {
@@ -13,8 +13,6 @@ export const AppRouter = ({ children }: Props) => {
         <BrowserRouter>
             <RoutesWithNotFound>
                 <Route path="/" element={<Navigate to={'/login'} />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/login" element={<Login />} />
                 {children}
             </RoutesWithNotFound>
